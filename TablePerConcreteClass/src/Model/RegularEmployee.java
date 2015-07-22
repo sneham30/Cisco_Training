946be7a21ee3b4214a0df9ee1086aa1c9f97d09f
sneham <sneham@DIN34000330.corp.capgemini.com>
2015-07-22 11:28:46 +0530

    Table per concrete class

---
+++ TablePerConcreteClass/src/Model/RegularEmployee.java
@@ -0,0 +1,21 @@
+package Model;
+
+
+public class RegularEmployee extends Employee {
+
+	private int salary;
+
+	/**
+	 * @return salary
+	 */
+	public int getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
